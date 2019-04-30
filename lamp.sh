1a2f45cd148b45223f68fdb192baf68f9fb471c2
yogisdm <31575108+yogisdm@users.noreply.github.com>
2019-04-30 11:50:07 +0530

    code fix

--- lamp.sh
+++ lamp.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 
-MYSQLPASSWORD= "Password@123"
-WPDBNAME= "blog"
-
-WPUSER= "root" 
-
-WPPWD= "Password@123"
+MYSQLPASSWORD="Password@123"
+WPDBNAME="blog"
+WPUSER="root" 
+WPPWD="Password@123"
 
 
 echo "Updating repo information...\n"
