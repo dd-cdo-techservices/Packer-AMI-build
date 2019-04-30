11cf850b35f7d64cb25baa12ea83bbfe1d9a480e
yogisdm <31575108+yogisdm@users.noreply.github.com>
2019-04-30 12:11:10 +0530

    code fix

--- lamp.sh
+++ lamp.sh
@@ -8,7 +8,7 @@ WPPWD="Password@123"
 
 echo "Updating repo information...\n"
 
-sudo apt-get -f autoremove
+sudo apt-get -y install -f
 sudo apt-get -y update 
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
